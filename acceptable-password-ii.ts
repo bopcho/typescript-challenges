38b12a9396454cd433d6f5c833e10e8d8e8ed7a8
Boyan Asenov <ivanoffboyan@gmail.com>
2023-05-20 20:07:29 +0200

    Add first-word.ts file

---
+++ acceptable-password-ii.ts
@@ -0,0 +1,15 @@
+// Taken from mission Acceptable Password I
+
+import assert from "assert";
+function isAcceptablePassword(password: string): boolean {
+   {
+    return password.length > 6 && /\d/.test(password);
+}
+
+
+    return password.length > 6;
+}
+
+
+console.log("Example:");
+console.log(isAcceptablePassword("short"));
